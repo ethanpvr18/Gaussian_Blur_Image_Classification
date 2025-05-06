9fb047ba98cb72133255d91f05569ad5b022507a
Ethan Pervere <ethan.pervere@gmail.com>
2025-05-05 23:49:18 -0400

    Update GaussianBlurImageClassification.py

--- GaussianBlurImageClassification.py
+++ GaussianBlurImageClassification.py
@@ -41,7 +41,7 @@ for image_path in images:
             modified = cv.GaussianBlur(img, (blur,blur), 0)
     
             # Load YOLO model
-            net = cv.dnn.readNetFromDarknet('yolov.cfg', 'yolov.weights')
+            net = cv.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
             net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
     
             # Get output layer names
