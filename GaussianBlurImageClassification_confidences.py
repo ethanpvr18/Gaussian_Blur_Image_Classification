4c4cc64c5d1ac3ec63d70b3e7948f8868deed549
Ethan Pervere <ethan.pervere@gmail.com>
2025-05-15 04:07:52 -0400

    Update GaussianBlurImageClassification_confidences.py

--- GaussianBlurImageClassification_confidences.py
+++ GaussianBlurImageClassification_confidences.py
@@ -129,8 +129,6 @@ for image_path in images:
 
 plt.xlabel("Kernel Size of Gaussian Blur")
 plt.ylabel("Confidence Level")
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.tight_layout()
 plt.grid(axis='both', which='minor')
 plt.title("Impact of Gaussian Blur on Image Classification Confidence")
 plt.savefig('plot_confidences.pdf')
