3e3bbc75513c6b915c7d8fec6b66729f3a336c37
Ethan Pervere <ethan.pervere@gmail.com>
2025-05-15 03:10:09 -0400

    Update GaussianBlurImageClassification.py

--- GaussianBlurImageClassification.py
+++ GaussianBlurImageClassification.py
@@ -133,7 +133,8 @@ for image_path in images:
 
 plt.xlabel("Kernel Size of Gaussian Blur")
 plt.ylabel("Percent of Objects Classified")
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.tight_layout()
 plt.grid(axis='both', which='minor')
 plt.title("Impact of Gaussian Blur on Image Classification")
 plt.savefig('plot.pdf')
