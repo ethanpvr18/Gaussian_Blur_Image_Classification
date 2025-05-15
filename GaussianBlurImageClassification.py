c4dda3eddb378548062b2dd82caee210e161f343
Ethan Pervere <ethan.pervere@gmail.com>
2025-05-14 23:43:00 -0400

    Update GaussianBlurImageClassification.py

--- GaussianBlurImageClassification.py
+++ GaussianBlurImageClassification.py
@@ -100,7 +100,8 @@ for image_path in images:
             if len(indices) > 0:
                 for i in indices.flatten():
                     x, y, w, h = boxes[i]
-                    label = f"{classes[class_ids[i]]}: {confidences[i]:.2f}"
+                    # label = f"{classes[class_ids[i]]}: {confidences[i]:.2f}"
+                    label = ""
                     color = (0, 255, 0)
         
                     cv.rectangle(modified, (x, y), (x + w, y + h), color, thickness)
